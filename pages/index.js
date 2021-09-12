1b5e738404a9e01c92cd9c8081ad632a7656123c
Théo HOLANDER <holandertheo@gmail.com>
2021-09-12 16:49:45 +0200

    feat: initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>blog</title>
+        <meta name="description" content="Description for blog" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>This new Next.js app has been reset!</h1>
+      </main>
+    </>
+  );
+}
